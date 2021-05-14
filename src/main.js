fc2de9d9452a249c329e0dd5d07e70c6f4ac2a3a
unknown <82100646@idbins.co.kr>
2021-05-14 16:52:56 +0900

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/plugins/vuetify'
+import '@/assets/styles/app.scss'
+import x2js from 'x2js'
 
+Vue.prototype.$x2js = new x2js()
 Vue.config.productionTip = false
 
 new Vue({
